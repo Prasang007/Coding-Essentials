d46a19e6ee784a5870908937b80413961ef9008d
prasang007 <prasang00@gmail.com>
2021-06-23 23:14:28 +0530

    GFG k parts subarray

---
+++ GFG k subarray.cpp
@@ -0,0 +1,26 @@
+// C++ implementation of the above approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to divide an array into k
+// parts such that the sum of difference
+// of every element with the maximum element
+// of that part is minimum
+int divideArray(int arr[], int n, int k)
+{
+	// Dp to store the values
+	
+}
+
+// Driver code
+int main()
+{
+	int arr[] = { 2, 9, 5, 4, 8, 3, 6 };
+	int n = sizeof(arr) / sizeof(int);
+	int k = 2;
+
+	cout << divideArray(arr, n, k) << "\n";
+
+	return 0;
+}
--- counting sort.cpp
+++ counting sort.cpp
@@ -1,6 +1,15 @@
+/**
+IP
+6
+[2,2,2,1,3,1,3]
+OP
+1 1 2 2 2 3 3 
+
+*/
 #include<iostream>
 using namespace std;
-int count[10], n, digit;
+int count[10], n, digit;  // count has the temp array used to store number of times a number repeats 
+
 int main ()
 {
     int maxi =0;
